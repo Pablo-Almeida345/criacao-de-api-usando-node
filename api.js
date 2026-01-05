8e40be76ebcbccbfb1180e15c3dd1f6c75fbaa6a
Pablo-Alemida345 <pabloalmeidadasilva@eaportal.org>
2026-01-05 19:33:21 -0300

    Criação do localhost

--- api.js
+++ api.js
@@ -1,5 +1,10 @@
 import express from "express";
 
 const app = express();
+const PORT = 3000;
 
-app.listen()
+
+
+app.listen(PORT, () => {
+    console.log(`O servidor esta rodando na porta ${PORT}`)
+})
